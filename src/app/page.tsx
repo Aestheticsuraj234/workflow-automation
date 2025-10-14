2709e772f9cd04972093074bfd01fb728542046e
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-10-14 13:22:12 +0530

    04 Better Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+
 import { Button } from '@/components/ui/button'
+import { authClient } from '@/lib/auth-client';
+import { requireAuth } from '@/lib/auth-utils';
+import { caller } from '@/trpc/server';
 import React from 'react'
 
 
-const Page = () => {
+const Page = async() => {
+ await requireAuth();
+ const data = await caller.getUsers();
+
   return (
     <div className='min-h-screen h-full flex flex-col items-center justify-center'>
-      <Button>Click me</Button>
+      {JSON.stringify(data , null, 2)}
+     
+
     </div>
   )
 }
